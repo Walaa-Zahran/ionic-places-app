86204c3e543879ba1f1dd34f7186a8aae42c5c16
walaazahran <walaazahran40@gmail.com>
2023-10-25 11:58:33 +0300

    Fetching static image url

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -4,7 +4,8 @@ import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
-import { map } from 'rxjs';
+import { map, of, switchMap } from 'rxjs';
+import { PlaceLocation } from 'src/app/places/location.model';
 
 @Component({
   selector: 'app-location-picker',
@@ -22,11 +23,19 @@ export class LocationPickerComponent implements OnInit {
         if (!modalData.data) {
           return
         }
-        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(
-          address => {
-            console.log(address);
-          }
-        )
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lat,
+          lng: modalData.data.lng,
+          address: null,
+          staticMapImageUrl: null
+        };
+        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
+          switchMap(address => {
+            pickedLocation.address = address;
+            return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+          })).subscribe(staticMapImageUrl => {
+            pickedLocation.staticMapImageUrl = staticMapImageUrl;
+          });
       })
       modaLEl.present();
     })
@@ -40,5 +49,8 @@ export class LocationPickerComponent implements OnInit {
       }
       return geoData.results[0].formatted_address;
     }));
+  }
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap&markers=color:red%7Clabel:Place%7C${lat},${lng}&key=${environment.googleMapsAPIKey}`
   }
 }
