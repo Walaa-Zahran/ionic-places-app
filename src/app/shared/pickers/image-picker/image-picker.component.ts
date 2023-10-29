3f1de56ac041694b6c5f133c6fba30a326b64ad3
walaazahran <walaazahran40@gmail.com>
2023-10-29 10:32:25 +0200

    Avoid Distorted Pictures

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,10 +1,13 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Capacitor,
+
+} from '@capacitor/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-import { Capacitor } from '@capacitor/core';
 @Component({
   selector: 'app-image-picker',
   templateUrl: './image-picker.component.html',
-  styleUrls: ['./image-picker.component.scss'],
+  styleUrls: ['./image-picker.component.scss']
 })
 export class ImagePickerComponent implements OnInit {
   @Output() imagePick = new EventEmitter<string>();
@@ -13,23 +16,28 @@ export class ImagePickerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
   onPickImage() {
+    if (!Capacitor.isPluginAvailable('Camera')) {
+      return;
+    }
     Camera.getPhoto({
       quality: 50,
       source: CameraSource.Prompt,
       correctOrientation: true,
       height: 320,
-      width: 200,
+      width: 600,
       resultType: CameraResultType.Base64
-    }).then((image) => {
-      if (image.base64String) {
-        this.selectedImage = image.base64String;
-        this.imagePick.emit(image.base64String);
-      }
-
-    }).catch((error: any) => {
-      console.log(error);
-      return false;
     })
+      .then(image => {
+        if (image.base64String) {
+          this.selectedImage = image.base64String;
+          this.imagePick.emit(image.base64String);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        return false;
+      });
   }
 }
